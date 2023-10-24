b635d0d67a04c426a4c4a01d476e96b2b9b34b7f
Vandita Chapadia <vandita.c@simformsolutions.com>
2023-10-24 14:35:44 +0530

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,22 @@
+import { config } from './config';
+import Logger from 'bunyan';
+import mongoose from 'mongoose';
+
+const log: Logger = config.createLogger('setupDatabase');
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Successfully connected to database.');
+      })
+      .catch((error) => {
+        log.error(error);
+        log.error('Error connecting to database');
+        return process.exit(1);
+      });
+  };
+  connect();
+  mongoose.connection.on('disconnected', connect);
+};
